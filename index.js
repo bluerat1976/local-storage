2ea04e3eb8f1643db8894cb0515d7d47bbbbeb81
bluerat1976 <osoblivaia76@mail.ru>
2019-04-16 13:50:08 +0300

    Improved a little bit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-var name = prompt('input your name');
+
+var button = document.getElementById('button');
+button.addEventListener('click', func); 
+    function func() {
+         var name = prompt('input your name');
          localStorage.setItem('key', name);
            var text = document.getElementById('greet');
         if (name.length > 2) {
             text.innerHTML = 'Hello test user!';
         } else {
+            
             text.innerHTML = 'Hello guest!';
         }
+    }
+   
+
+
+
